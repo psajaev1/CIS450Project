dcc15d4d79762aaadc1e1dd513c5e2e274a6d753
psajaev1 <psajaev@gmail.com>
2020-12-14 15:19:14 -0600

    update cities

--- hw2/client/src/components/RecommendationsRow.js
+++ hw2/client/src/components/RecommendationsRow.js
@@ -8,11 +8,9 @@ export default class RecommendationsRow extends React.Component {
 
 	render() {
 		return (
-			<div className="movieResults">
-				<div className="title">{this.props.title}</div>
-				<div className="id">{this.props.id}</div>
-				<div className="rating">{this.props.rating}</div>
-				<div className="votes">{this.props.votes}</div>
+			<div className="cityResults">
+				<div className="airport">{this.props.airport}</div>
+				<div className="city">{this.props.City}</div>
 			</div>
 		);
 	}
